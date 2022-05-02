dfb8af958bfe0f388033bb004bc2a42f050c9bf4
S. Grimault <sebastien.grimault@gmail.com>
2022-05-02 22:23:45 +0200

    chore: clean correctly all git submodules and delete any modified file

--- upgrade_submodules.sh
+++ upgrade_submodules.sh
@@ -3,7 +3,7 @@
 APP_HOME="`pwd -P`"
 
 git submodule foreach git reset --hard HEAD
-git submodule foreach git clean -f
+git submodule foreach git clean -fxd
 git submodule update --remote
 git submodule foreach git checkout develop
 git submodule foreach git pull origin develop
